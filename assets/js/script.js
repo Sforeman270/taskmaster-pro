8c6678973188007dc7cd67f47f0ae5eb8f92abe8
Sforeman270 <65334884+Sforeman270@users.noreply.github.com>
2020-07-20 15:41:36 -0500

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,18 @@ var tasks = {};
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
-  var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(taskDate);
-  var taskP = $("<p>")
-    .addClass("m-1")
-    .text(taskText);
+  var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(taskDate);
+    
+    
+  var taskP = $("<p>").addClass("m-1").text(taskText);
 
+    
+   
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -260,6 +262,25 @@ $("#remove-tasks").on("click", function() {
 $("#modalDueDate").datepicker({
   minDate: 1
 });
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+  // ensure it worked
+  console.log(date);
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+   if (moment().isAfter(time)) {
+     $(taskEl).addClass("list-group-item-danger");
+   }
+
+};
 
 // load tasks for the first time
 loadTasks();
